f10e11bb9440f3421bc2e4f671fb9b3c3d15b7fe
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-04-18 01:18:56 +0500

    sections

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/sections/Header";
+import Footer from "@/sections/Footer";
 
 
 export const metadata: Metadata = {
@@ -14,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <>
+      <Header />
        {children}
+      <Footer />
     </>
   );
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,28 @@
+import Collections from '@/sections/Collections'
+import FAQ from '@/sections/FAQ'
+import Gallery from '@/sections/Gallery'
+import Hero from '@/sections/Hero'
+import HotSellers from '@/sections/HotSellers'
+import Inspiration from '@/sections/Inspiration'
+import NewArrival from '@/sections/NewArrival'
+import Reviews from '@/sections/Reviews'
+import Story from '@/sections/Story'
+import WhyUs from '@/sections/WhyUs'
 import React from 'react'
 
 const Home = () => {
   return (
     <main>
-      
+      <Hero />
+      <Story />
+      <Collections />
+      <NewArrival />
+      <HotSellers />
+      <WhyUs />
+      <Inspiration />
+      <Gallery />
+      <Reviews />
+      <FAQ />
     </main>
   )
 }
