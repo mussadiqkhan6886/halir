9d655a2fccf4c44f59d9d0692ccdc4c977336a57
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-04-18 02:10:15 +0500

    dont font

---
+++ lib/font.ts
@@ -0,0 +1,12 @@
+import {Inter} from "next/font/google"
+import localFont from "next/font/local";
+    
+export const inter = Inter({
+    weight: ["100","200","300","400","500","600","700"],
+    style: ["italic", "normal"],
+    subsets: ["latin"]
+})
+export const porcelain = localFont({
+  src: "../public/Magda Text Regular.otf",
+  variable: "--font-magda",
+});
