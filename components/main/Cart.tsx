68c4b6be1f62aad6a07a7b9d4638de566118b4b1
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-04-20 20:02:24 +0500

    cart

--- components/main/Cart.tsx
+++ components/main/Cart.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+'use client';
+
+import Image from 'next/image';
+import React, { useState } from 'react'
+import { FiX } from 'react-icons/fi';
 
 const Cart = () => {
+    const [cart, setCart] = useState(false)
   return (
     <div>
-      
+      <div className='flex justify-between items-center'>
+        <h5>My cart</h5>
+        <FiX />
+      </div>
+      <div>
+        <div>
+            <Image src={"/men.jpg"} alt='cart' width={200} height={200} />
+        </div>
+        <div>
+            <div>
+                <h6>VIOLETTE 30</h6>
+                <p>Perfume</p>
+            </div>
+            <div>   
+                <p>Price: <span>2500 PKR</span></p>
+                <p>Size: <span>50ml</span></p>
+                <p>Quantity: <button>-</button><span>1</span><button>+</button></p>
+            </div>
+        </div>
+      </div>
+      <div>
+        <div>
+            <p>Sub total:</p>
+            <p>PKR 2500</p>
+        </div>
+        <div>
+            <button>CHECKOUT</button>
+        </div>
+      </div>
     </div>
   )
 }
