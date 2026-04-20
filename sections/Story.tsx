6a9b7f767d9350de49980795e7aa9a7b32c454df
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-04-20 20:43:03 +0500

    fixed story

--- sections/Story.tsx
+++ sections/Story.tsx
@@ -9,7 +9,7 @@ const Story = () => {
       <Image src={"/story3.jpg"} alt='perfume image' width={200} height={300} className='object-cover object-center hidden lg:block absolute top-0 right-0' />
 
       <div className='flex justify-center'>
-        <h3 className='text-3xl md:text-5xl px-2 pt-20 relative leading-10 tracking-tight'> 
+        <h3 className='text-3xl md:text-5xl px-2 pt-10 md:pt-20 relative leading-10 tracking-tight'> 
           <span className='inline-block md:ml-30'>WE <span className={`${ekate.className} md:text-8xl text-5xl`}>Create </span> NOT JUST </span>
           <br />
           A PERFUME, <span className='inline md:hidden'>BUT A</span> <span className='hidden md:inline-block ml-28 md:ml-36'>BUT A</span> <br />
@@ -19,7 +19,7 @@ const Story = () => {
            </span>
          </h3>
       </div>
-      <div className='grid p-3 text-zinc-700 grid-cols-2  gap-10 text-sm max-w-3xl mx-auto my-10 text-justify'>
+      <div className='grid p-3 text-zinc-700 grid-cols-1 md:grid-cols-2  gap-10 text-sm max-w-3xl mx-auto my-10 text-justify'>
         <div>
           <p>Halir was born from a deep passion for refined, premium fragrances a passion to create scents that do more than just smell good. We believe a fragrance is a personal signature, an invisible expression of identity that leaves a lasting impression.
 
