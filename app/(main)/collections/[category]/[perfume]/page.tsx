f1e11b45b654581e3e6e55aa17c9efe3c173752b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-04-21 17:44:51 +0500

    perfume page done

--- app/(main)/collections/[category]/[perfume]/page.tsx
+++ app/(main)/collections/[category]/[perfume]/page.tsx
@@ -9,23 +9,24 @@ const page = async ({params}: {params: Promise<{perfume: string, category: strin
   const {perfume, category} = await params
   const currentPerfume = perfumes.find(item => item.slug === perfume)
   return (
-    <main className='max-w-6xl mx-auto'>
-      <section className='bg-zinc-200 max-w-8xl mx-auto'>
+    <main className='max-w-6xl py-3 mx-auto'>
+      <section className='bg-light max-w-8xl mx-auto'>
         {/* breadcrumps */}
        <div className={`${magdaLig.className} flex gap-1 items-center text-sm`}>
           <Link href={"/"}>Home</Link>
           <span>/</span>
-          <Link className='capitalize' href={`/${category}`}>{category.replace("-", " ")}</Link>
+          <Link className='capitalize hover:underline' href={`/${category}`}>{category.replace("-", " ")}</Link>
           <span>/</span>
-          <Link className='capitalize' href={`/${category}/${perfume}`}>{perfume.replace("-", " ")}</Link>
+          <Link className='capitalize hover:underline' href={`/${category}/${perfume}`}>{perfume.replace("-", " ")}</Link>
         </div>
-        <div>
-          <h1>{currentPerfume?.name}</h1>
-          <p>{currentPerfume?.description}</p>
+        <div className='my-3'>
+          <h1 className='uppercase font-black tracking-wide text-2xl md:text-xl'>{currentPerfume?.name}</h1>
+          <p className={`${magdaLig.className} text-stone-600 text-sm`}>{currentPerfume?.description}</p>
         </div>
       </section>
       <section className='relative max-w-6xl mx-auto w-full h-[350px]'>
         <Image src={currentPerfume?.mainImage as string} alt={currentPerfume?.name as string} fill className='object-center object-cover' />
+        <h2 className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-black uppercase text-xl md:text-2xl'>{currentPerfume?.name}</h2>
       </section>
       <section className='grid py-24 grid-cols-4 gap-5'>
         {currentPerfume?.sizes.map(item => (
